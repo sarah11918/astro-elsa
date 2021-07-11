803b117fd82c55d4aa81765b4bf15966b2922066
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-07-11 14:10:35 -0300

    Improved the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,15 @@ import React, {useState} from 'react';
 
 const Button = ({ children }) => {
   const [status, setStatus] = useState(true)
-  return (<>
-    <h2>Toggle Button</h2>
-    <button onClick={() => setStatus(!status)}>{status ? "True" : "False"}</button>
-  </>)
+  return (
+    <>
+      <h2>Toggle Button</h2>
+      <button onClick={() => setStatus(!status)}>Press to Toggle</button>
+      
+      {' '}{status ? "True" : "False"}
+     
+    </>
+  )
 }
 
 export default Button;
