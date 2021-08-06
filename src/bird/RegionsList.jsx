3fc46340a1a318b6b7355e31edc9bee36105c197
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-08-06 12:03:23 -0300

    dependency updates, plus key value in Regions list

--- src/bird/RegionsList.jsx
+++ src/bird/RegionsList.jsx
@@ -1,14 +1,21 @@
-import React from "react"
+import React from "react";
 
-export default function RegionsList(props){
+export default function RegionsList(props) {
+  let regionDivs = props.regionData.map((region) => (
+    <li key={region.code} className="regionItem">
+      {region.name}: {region.code}
+    </li>
+  ));
 
-  let regionDivs = props.regionData.map(region => <li className="regionItem">{region.name}: {region.code}</li>)
-
-  return(
+  return (
     <>
       <h3>All subregions</h3>
       <ul>{regionDivs}</ul>
-      <p className = "more-info"><a href="https://ebird.org/canada/region/world/regions?yr=all&m=&hsStats_sortBy=cl&hsStats_o=desc">You can also explore eBird.org to find your region ID</a></p>
+      <p className="more-info">
+        <a href="https://ebird.org/canada/region/world/regions?yr=all&m=&hsStats_sortBy=cl&hsStats_o=desc">
+          You can also explore eBird.org to find your region ID
+        </a>
+      </p>
     </>
-  )
+  );
 }
