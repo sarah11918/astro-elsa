d12d3bae0363b39ddd6b7b20aa9d64e8ec18603a
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-08-09 17:28:57 -0300

    Find hot birds in your area!

--- src/bird/Recent.jsx
+++ src/bird/Recent.jsx
@@ -3,10 +3,13 @@ import { useState } from "react";
 import RecentBirdList from "./RecentBirdList.js";
 
 export default function Recent() {
-  const [recentBirds, setRecentBirds] = useState([{ comName: "" }]);
-  const [location, setLocation] = useState("CA-PE-PR");
+  const [recentBirds, setRecentBirds] = useState([]);
+  const [location, setLocation] = useState("");
 
-  async function getSightings() {
+  async function getSightings(event) {
+    event.preventDefault();
+    setLocation(event.target.elements.location.value)
+    const recentUrl = `https://api.ebird.org/v2/data/obs/${event.target.elements.location.value}/recent?back=14`
     const myHeaders = new Headers();
     myHeaders.append("X-eBirdApiToken", "2ifbkhv7g8ct");
 
@@ -16,28 +19,22 @@ export default function Recent() {
       redirect: "follow"
     };
     const response = await fetch(
-      `https://api.ebird.org/v2/data/obs/${location}/recent?back=14`,
+      recentUrl,
       requestOptions
     );
     const data = await response.json();
     setRecentBirds(data);
   }
 
-  function changeLocation(event){
-    event.preventDefault()
-    setLocation(event.target.elements.location.value)
-  }
-
   return (
     <>
-      <h3>Get a list of recent birds in your area!</h3>
+      <h3>Get a list of recently-observed birds in your area!</h3>
       <h5 className="birdtab">Current location set: {location} </h5>
 
-      <form onSubmit={changeLocation}>
+      <form onSubmit={getSightings}>
         <input name="location" type="text" placeholder="eBird region ID eg. CA-PE"/>
-        <button> Set Location </button>
+        <button> See the birds!</button>
       </form>
-      <button onClick={getSightings}>Get the list of recent birds</button>
       <RecentBirdList birdList={recentBirds} />
     </>
   );
